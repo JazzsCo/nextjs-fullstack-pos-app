e0096d0aed0f6594253f5e6c6cb9219a92e0f1a0
Jzz-C <ygyi8632@gmail.com>
2023-04-26 08:20:01 +0630

    feat: added other feat in menus repo

--- pages/routes/menus/[id].tsx
+++ pages/routes/menus/[id].tsx
@@ -4,7 +4,22 @@ import { AppContext } from "@/pages/contexts/AppContext";
 import { Menus } from "@/pages/typings/types";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
-import ButtonSide from "./ButtonSide";
+import { Button, Box, Modal } from "@mui/material";
+import axios from "axios";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  borderRadius: 2,
+  borderColor: "white",
+  p: 4,
+};
 
 const MenuById = () => {
   const { menus, fetchData } = useContext(AppContext);
@@ -16,7 +31,33 @@ const MenuById = () => {
 
   useEffect(() => {
     setMenu(menus.find((menu) => menu.id.toString() === id?.toString()));
-  }, [menu?.id]);
+    fetchData();
+  }, [menus]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const menu = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+    };
+    await axios
+      .put(`/api/menusPost?id=${id}`, {
+        menu,
+      })
+      .then((res) => {
+        console.log(res.data);
+        return res;
+      })
+      .catch((err) => {
+        return err;
+      });
+    // fetchData();
+  };
 
   return (
     <Layout>
@@ -92,7 +133,56 @@ const MenuById = () => {
               <span className="text-3xl font-bold text-gray-900 dark:text-white">
                 ${menu?.price}
               </span>
-              <ButtonSide menu={menu} fetchData={fetchData} />
+              <div>
+                <Button variant="outlined" onClick={handleOpen}>
+                  Update Menu
+                </Button>
+
+                <Modal
+                  sx={{}}
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <form onSubmit={handleSubmit}>
+                      <div className="mb-6">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                          Name
+                        </label>
+                        <input
+                          type="text"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          defaultValue={menu?.name}
+                          name="name"
+                          required
+                        />
+                      </div>
+                      <div className="mb-6">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                          Price
+                        </label>
+                        <input
+                          type="number"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          defaultValue={menu?.price}
+                          name="price"
+                          required
+                        />
+                      </div>
+                      <div className="flex justify-end">
+                        <button
+                          type="submit"
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                          Update
+                        </button>
+                      </div>
+                    </form>
+                  </Box>
+                </Modal>
+              </div>
             </div>
           </div>
         </div>
