feccefc6666e22751586ab36884be2a24f04c382
Jzz-C <ygyi8632@gmail.com>
2023-05-20 11:02:10 +0630

    feat: added fileUpload component

---
+++ libs/fileUpload.ts
@@ -0,0 +1,31 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import multer from "multer";
+import multerS3 from "multer-s3";
+
+const endpoint = process.env.SPACE_ENDPOINT || "";
+const accessKey = process.env.SPACE_ACCESS_KEY_ID || "";
+const secretKey = process.env.SPACE_SECRET_ACCESS_KEY || "";
+
+console.log(endpoint, accessKey, secretKey);
+
+// Set S3 endpoint to DigitalOcean Spaces
+const s3Config = new S3Client({
+  endpoint: endpoint,
+  region: "sgp1",
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+  },
+});
+
+// Change bucket property to your Space name
+export const fileUpload = multer({
+  storage: multerS3({
+    s3: s3Config,
+    bucket: "msquarefdc",
+    acl: "public-read",
+    key: function (request, file, cb) {
+      cb(null, `happy-pos/jzzz/${Date.now()}_${file.originalname}`);
+    },
+  }),
+}).array("files", 1);
---
+++ pages/api/assets/index.ts
@@ -0,0 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { fileUpload } from "@/libs/fileUpload";
+import { Request, Response } from "express";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+type CustomNextApiRequest = NextApiRequest &
+  Request & {
+    files: any[];
+  };
+
+type CustomNextApiResponse = NextApiResponse & Response;
+
+export default async function handler(
+  req: CustomNextApiRequest,
+  res: CustomNextApiResponse
+) {
+  try {
+    fileUpload(req, res, async (error) => {
+      if (error) {
+        return res.send(500);
+      }
+      const files = req.files as Express.MulterS3.File[];
+      const file = files[0];
+      const assetUrl = file.location;
+      res.send({ assetUrl });
+    });
+  } catch (err) {
+    res.send(500);
+  }
+}
