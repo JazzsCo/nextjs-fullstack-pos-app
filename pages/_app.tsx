6cb57a31bfd339307f7c9969c9cee6f85ad9d010
Jzz-C <ygyi8632@gmail.com>
2023-07-11 08:55:59 +0630

    feat: set up redux toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "react-redux";
 
 import AdminProvider from "@/contexts/AdminContext";
 import OrderProvider from "@/contexts/OrderContext";
+import { store } from "./store";
 
 const queryClient = new QueryClient();
 
@@ -13,13 +15,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={pageProps.session}>
-        <AdminProvider>
-          <OrderProvider>
-            <ThemeProvider>
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </OrderProvider>
-        </AdminProvider>
+        {/* <AdminProvider>
+          <OrderProvider> */}
+        <Provider store={store}>
+          <ThemeProvider>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Provider>
+        {/* </OrderProvider>
+        </AdminProvider> */}
       </SessionProvider>
     </QueryClientProvider>
   );
---
+++ pages/store/slices/counterSlice.ts
@@ -0,0 +1,63 @@
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../";
+
+// Define a type for the slice state
+interface CounterState {
+  isLoading: boolean;
+  value: number;
+  data: any;
+}
+
+// Define the initial state using that type
+const initialState: CounterState = {
+  isLoading: false,
+  value: 0,
+  data: {},
+};
+
+export const fetchContent = createAsyncThunk(
+  "content/fetchContent",
+  async () => {
+    const response = await fetch(`/api/order?locationId=4`);
+    const responseJson = await response.json();
+    return responseJson;
+  }
+);
+
+export const counterSlice = createSlice({
+  name: "counter",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchContent.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchContent.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchContent.rejected, (state, action) => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer;
